1b4092c0e202fb7109fc79cf7225fdabd6d2187b
Saidul Mursalin <47191910+rocktohq@users.noreply.github.com>
2022-12-04 17:08:12 +0600

    Update footer.php
    Footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 <footer class="bg-light mt-5 p-3">
 	<div class="container">
 		<p class="text-center">&copy; Copyright <span class="text-success">bd</span><span class="text-danger">Tracker</span>&trade; 2022. All right reserved.</p>
-		<p class="text-center">by <span class="text-danger">Team bdTracker</span> & Supervised by <span class="text-danger">Mosharraf Hossain Pranto</span>.</p>
+		<p class="text-center">by <span class="text-danger">Saidul Mursalin</span>.</p>
 	</div>
 </footer>
 <!-- FOOTER./ -->
